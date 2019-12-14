23c6bf02c041010e04eb12c6e8cf2a72af54d1e5
We-Gold <we.goldm@gmail.com>
2019-12-14 09:54:06 -0500

    Specified imports for the training file

---
+++ train.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
