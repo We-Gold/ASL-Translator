00c5b4b7231f1b5d0422f865d2f31bc67301254b
We-Gold <we.goldm@gmail.com>
2019-12-14 18:22:39 -0500

    Added comments to the training file.

--- transfer_train.py
+++ transfer_train.py
@@ -12,45 +12,45 @@ from keras.models import Model
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 
+# Download the mobile net pretrained base.
+base_model = MobileNet(weights = 'imagenet',include_top = False) 
 
-base_model=MobileNet(weights='imagenet',include_top=False) 
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(512,activation = 'relu')(x) 
+x = Dense(256,activation = 'relu')(x)
+preds = Dense(29,activation = 'softmax')(x)
 
-x=base_model.output
-x=GlobalAveragePooling2D()(x)
-x=Dense(512,activation='relu')(x) 
-x=Dense(256,activation='relu')(x)
-preds=Dense(29,activation='softmax')(x)
-
-
-model=Model(inputs=base_model.input,outputs=preds)
+# Initialize the full model.
+model = Model(inputs = base_model.input,outputs = preds)
 
 for layer in model.layers[:20]:
-    layer.trainable=False
+    layer.trainable = False
 for layer in model.layers[20:]:
-    layer.trainable=True
+    layer.trainable = True
 
+# Setup the data generator, and preprocess the data.
+train_datagen = ImageDataGenerator(rescale = 1./255, rotation_range = 15, width_shift_range = 0.25, height_shift_range = 0.25)
 
-#train_datagen=ImageDataGenerator(preprocessing_function=preprocess_input) 
-train_datagen=ImageDataGenerator(rescale=1./255, rotation_range=15, width_shift_range=0.25, height_shift_range=0.25)
+train_generator = train_datagen.flow_from_directory('data/train',
+                                                 target_size = (200,200),
+                                                 color_mode = 'rgb',
+                                                 batch_size = 32,
+                                                 class_mode = 'categorical',
+                                                 shuffle = True)
 
-train_generator=train_datagen.flow_from_directory('data/train',
-                                                 target_size=(200,200),
-                                                 color_mode='rgb',
-                                                 batch_size=32,
-                                                 class_mode='categorical',
-                                                 shuffle=True)
-
-model.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'])
+model.compile(optimizer = 'Adam',loss = 'categorical_crossentropy',metrics = ['accuracy'])
 
 checkpoints = ModelCheckpoint("checkpoints/weights.{epoch:02d}.h5",
-                                          save_weights_only=False,
-                                          verbose=1)
+                                          save_weights_only = False,
+                                          verbose = 1)
 
-step_size_train=train_generator.n//train_generator.batch_size
+step_size_train = train_generator.n//train_generator.batch_size
 
-model.fit_generator(generator=train_generator,
-                   steps_per_epoch=step_size_train,
-                   epochs=5,
-                   callbacks=[checkpoints])
+# Train and save the model
+model.fit_generator(generator = train_generator,
+                   steps_per_epoch = step_size_train,
+                   epochs = 5,
+                   callbacks = [checkpoints])
 
 model.save("model1.h5")
