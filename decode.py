23d4324b5ecb14c9be4eabe3dac66a9624e4ea47
Anirudh Rao <emptyresearch@outlook.com>
2019-12-14 17:47:37 -0500

    Fixed decoder

--- decode.py
+++ decode.py
@@ -8,8 +8,10 @@ window = tkinter.Tk()
 webcam = cv2.VideoCapture(0)
 
 webcamFrame = tkinter.Frame(window, width=1280, height=720)
-lmain = tkinter.Label(window)
-lmain.pack()
+webcamLabel = tkinter.Label(window)
+webcamLabel.pack()
+aslCharText = tkinter.Text(window, height=2, width=30)
+aslCharText.pack()
 
 frameCount = 0
 def update():
@@ -19,15 +21,15 @@ def update():
     imgtk = ImageTk.PhotoImage(
             image=Image.fromarray(
                 cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)))
-    lmain.imgtk = imgtk 
-    lmain.configure(image=imgtk)
+    webcamLabel.imgtk = imgtk 
+    webcamLabel.configure(image=imgtk)
 
     if frameCount >= 15:
         frameCount = 0
         cv2.imwrite('asl.jpg', frame)
         print(model.aslToChar('asl.jpg'), end="", flush=True)
 
-    lmain.after(16, update)
+    webcamLabel.after(16, update)
     frameCount += 1
 
 update()
